42b11d30603887c92934077cd933c1e65e6393eb
Koki Takahashi <hakatasiloving@gmail.com>
2019-07-01 15:07:59 +0900

    Render pins

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,6 @@
 <template>
 	<div class="container">
-		<div>
-			<logo/>
-			<h1 class="title">
-				tokyo-olym.pics
-			</h1>
-			<h2 class="subtitle">
-				You see?
-			</h2>
-			<div class="links">
-				<a href="https://nuxtjs.org/" target="_blank" class="button--green">
-					Documentation
-				</a>
-				<a
-					href="https://github.com/nuxt/nuxt.js"
-					target="_blank"
-					class="button--grey"
-				>
-					GitHub
-				</a>
-			</div>
-		</div>
+		<logo/>
 	</div>
 </template>
 
@@ -42,27 +22,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
