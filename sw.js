3b48a20a19e9386a3676863bc0a536924ea21dfe
Koki Takahashi <hakatasiloving@gmail.com>
2019-07-02 21:02:07 +0900

    Update build

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "0b380b69230bb832704cd80f72877ba6"
   },
   {
-    "url": "/_nuxt/6e5ed05551adff8a5245.js",
-    "revision": "de18776b45d2823c71ca4048c5d4b97e"
+    "url": "/_nuxt/607d8ec19441fc05ec69.js",
+    "revision": "f3fc6458e62ca8cb25644233a950781a"
   },
   {
     "url": "/_nuxt/7b3f5f7caf22a0383fd9.js",
@@ -18,8 +18,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "2516193ca0ef816cdb0540a4cb573ccd"
   },
   {
-    "url": "/_nuxt/8e84333c450614eff4b0.js",
-    "revision": "6b75c439bd6cca24259961c1abc76f35"
+    "url": "/_nuxt/92d47c3a565d3fb87568.js",
+    "revision": "4d29e861f074846d8e97031dddfb70a2"
   }
 ], {
   "cacheId": "tokyo-olym.pics",
