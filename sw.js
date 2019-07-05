7e9cc2b4a0842a67f28a600c2b89641d633be542
Koki Takahashi <hakatasiloving@gmail.com>
2019-07-06 02:58:41 +0900

    Update build

--- sw.js
+++ sw.js
@@ -2,12 +2,8 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0146f692e33a2ddd4661.js",
-    "revision": "d2f2ed4be3ffb0d73255e527fe82ab16"
-  },
-  {
-    "url": "/_nuxt/3993aed7f9c0f3cada7a.js",
-    "revision": "f6387eacc10756a803e29e2e2eabe419"
+    "url": "/_nuxt/4179f3b329dc20bbb673.js",
+    "revision": "1b9607d2f2216a418e4bfeaed3db0af0"
   },
   {
     "url": "/_nuxt/67bef6cbf95143792b40.js",
@@ -16,6 +12,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/8fdaf06a06f8340284b7.js",
     "revision": "ec15d3b6a1d902ad407f53e279831d7b"
+  },
+  {
+    "url": "/_nuxt/d23e50812ef8053d65b1.js",
+    "revision": "d19b75452da194097dff613b35446419"
   },
   {
     "url": "/_nuxt/dd512419716350d35498.js",
